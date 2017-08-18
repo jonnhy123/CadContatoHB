4d6495f20e0910d1bf1d160ff2331edffa5a035c
jmattana <jhonatan_mattana@outlook.com>
2017-08-17 22:26:18 -0300

    final

--- src/main/java/br/univel/Contato.java
+++ src/main/java/br/univel/Contato.java
@@ -2,11 +2,14 @@ package br.univel;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Contato {
+	
 	@Id
+	@GeneratedValue
 	private int id;
 	@Column
 	private String nome;
